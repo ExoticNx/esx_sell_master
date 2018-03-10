8f5888e2230efce6849116eebdfb27faae70ed09
ExoticNx <ExoticNx@hotmail.com>
2018-03-09 18:00:53 -0800

    Initial commit

---
+++ esx_sellCleanMoney/esx_sell/server.lua
@@ -0,0 +1,143 @@
+ESX = nil
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local selling = false
+	local success = false
+	local paymentc = math.random (500,1000)
+	local paymentw = math.random (150,300)
+	local paymentm = math.random (300,700)
+	local paymento = math.random (150,300)
+  
+  RegisterNetEvent('drugs:trigger')
+  AddEventHandler('drugs:trigger', function()
+	TriggerEvent('currentlySelling')
+	    if selling == true then
+			TriggerEvent('pass_or_fail')
+  			TriggerClientEvent("pNotify:SetQueueMax", source, "lmao", 1)
+  			TriggerClientEvent("pNotify:SendNotification", source, {
+            text = "trying to convince person to buy the product",
+            type = "error",
+            queue = "lmao",
+            timeout = 2500,
+            layout = "Centerleft"
+        	})
+ 	end
+end)
+
+  RegisterNetEvent('drugs:sell')
+  AddEventHandler('drugs:sell', function()
+  	local xPlayer = ESX.GetPlayerFromId(source)
+	local meth = xPlayer.getInventoryItem('meth_pooch').count
+	local coke 	  = xPlayer.getInventoryItem('coke_pooch').count
+	local weed = xPlayer.getInventoryItem('weed_pooch').count
+	local opium = xPlayer.getInventoryItem('opium_pooch').count
+
+		if coke >= 1 and success == true then
+			 	TriggerClientEvent("pNotify:SetQueueMax", source, "lmao", 5)
+				TriggerClientEvent("pNotify:SendNotification", source, {
+					text = "You have sold a bag of cocaine for $" .. paymentc ,
+					type = "success",
+					progressBar = false,
+					queue = "lmao",
+					timeout = 2000,
+					layout = "CenterLeft"
+			})
+			xPlayer.removeInventoryItem('coke_pooch', 1)
+  			xPlayer.addMoney(paymentc)
+  			selling = false
+  		elseif weed >= 1 and success == true then
+  				TriggerClientEvent("pNotify:SetQueueMax", source, "lmao", 5)
+				TriggerClientEvent("pNotify:SendNotification", source, {
+					text = "You have sold a joints for $" .. paymentw ,
+					type = "success",
+					progressBar = false,
+					queue = "lmao",
+					timeout = 2000,
+					layout = "CenterLeft"
+			})
+  			xPlayer.removeInventoryItem('weed_pooch', 1)
+  			xPlayer.addMoney(paymentw)
+  			selling = false
+  		  elseif meth >= 1 and success == true then
+  				TriggerClientEvent("pNotify:SetQueueMax", source, "lmao", 5)
+				TriggerClientEvent("pNotify:SendNotification", source, {
+					text = "You have sold a methbag for $" .. paymentm ,
+					type = "success",
+					progressBar = false,
+					queue = "lmao",
+					timeout = 2000,
+					layout = "CenterLeft"
+			})
+  			xPlayer.removeInventoryItem('meth_pooch', 1)
+  			xPlayer.addMoney(paymentm)
+  			selling = false
+  			elseif opium >= 1 and success == true then
+  				TriggerClientEvent("pNotify:SetQueueMax", source, "lmao", 5)
+				TriggerClientEvent("pNotify:SendNotification", source, {
+					text = "You have sold a bag of heroin for $" .. paymento ,
+					type = "success",
+					progressBar = false,
+					queue = "lmao",
+					timeout = 2000,
+					layout = "CenterLeft"
+			})
+			xPlayer.removeInventoryItem('opium_pooch', 1)
+  			xPlayer.addMoney(paymento)
+  			selling = false
+			elseif selling == true and success == false then
+				TriggerClientEvent("pNotify:SetQueueMax", source, "lmao", 5)
+				TriggerClientEvent("pNotify:SendNotification", source, {
+					text = "They are not interested",
+					type = "error",
+					progressBar = false,
+					queue = "lmao",
+					timeout = 2000,
+					layout = "CenterLeft"
+			})
+  			selling = false
+  		end
+end)
+
+RegisterNetEvent('currentlySelling')
+AddEventHandler('currentlySelling', function()
+	selling = true
+end)
+
+RegisterNetEvent('pass_or_fail')
+AddEventHandler('pass_or_fail', function()
+  		
+  		local percent = math.random(1, 10)
+
+  		if percent == 8 or percent == 9 or percent == 10 then
+  			success = false
+  		elseif percent ~= 8 or percent ~= 9 or percent ~= 10 then
+  			success = true
+  		end
+  end)
+
+RegisterNetEvent('sell_dis')
+AddEventHandler('sell_dis', function()
+		TriggerClientEvent("pNotify:SetQueueMax", source, "lmao", 5)
+		TriggerClientEvent("pNotify:SendNotification", source, {
+		text = "You moved to far away",
+		type = "error",
+		progressBar = false,
+		queue = "lmao",
+		timeout = 2000,
+		layout = "CenterLeft"
+	})
+end)
+
+RegisterNetEvent('nodr')
+AddEventHandler('nodr', function()
+		TriggerClientEvent("pNotify:SetQueueMax", source, "lmao", 5)
+		TriggerClientEvent("pNotify:SendNotification", source, {
+		text = "You dont have any drugs",
+		type = "error",
+		progressBar = false,
+		queue = "lmao",
+		timeout = 2000,
+		layout = "CenterLeft"
+	})
+end)
---
+++ esx_sellDirtyMoney/esx_sell/client.lua
@@ -0,0 +1,58 @@
+ESX                 = nil
+local selling 	    = false
+
+Citizen.CreateThread(function()
+  while ESX == nil do
+    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+    Citizen.Wait(0)
+  end
+end)
+
+currentped = nil
+Citizen.CreateThread(function()
+
+while true do
+  Wait(0)
+  local player = GetPlayerPed(-1)
+  local playerloc = GetEntityCoords(player, 0)
+  local handle, ped = FindFirstPed()
+  repeat
+    success, ped = FindNextPed(handle)
+    local pos = GetEntityCoords(ped)
+    local distance = GetDistanceBetweenCoords(pos.x, pos.y, pos.z, playerloc['x'], playerloc['y'], playerloc['z'], true)
+    if IsPedInAnyVehicle(GetPlayerPed(-1)) == false then
+      if DoesEntityExist(ped)then
+        if IsPedDeadOrDying(ped) == false then
+          if IsPedInAnyVehicle(ped) == false then
+            local pedType = GetPedType(ped)
+            if pedType ~= 28 and IsPedAPlayer(ped) == false then
+              currentped = pos
+              if distance <= 2 and ped  ~= GetPlayerPed(-1) and ped ~= oldped then
+                if IsControlJustPressed(1, 74) then
+                      oldped = ped
+                      SetEntityAsMissionEntity(ped)
+                      TaskStandStill(ped, 9.0)
+                      pos1 = GetEntityCoords(ped)
+                      TriggerServerEvent('drugs:trigger')
+                      Citizen.Wait(2850)
+                      if distance <= 2 and ped  ~= GetPlayerPed(-1) and ped ~= oldped then
+                      TriggerServerEvent('drugs:sell')
+                      SetPedAsNoLongerNeeded(oldped)
+                  	  elseif distance > 2 and ped  ~= GetPlayerPed(-1) and ped ~= oldped then
+                  	  TriggerServerEvent('sell_dis')
+                  	  SetPedAsNoLongerNeeded(oldped)
+                  	  else
+                  	  TriggerServerEvent('nodr')
+                  	  SetPedAsNoLongerNeeded(oldped)
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  until not success
+  EndFindPed(handle)
+end
+end)
